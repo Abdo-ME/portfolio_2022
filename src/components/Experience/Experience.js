fde4e05c65aa0247c2df9ebcfc8d5968edbe5523
Abdo-ME <abdelhakmehda@gmail.com>
2022-06-20 07:48:31 +0100

    add responsivity to the site

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './experience.css'
 const Experience = () => {
     return (
-        <div className='experience'>
+        <div className='experience' id='experience'>
             <div className="achievement">
                 <div className="circle">+2</div>
                 <span>years</span>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,11 +12,21 @@ const Navbar = () => {
             <div className="n-right">
                 <div className="n-list">
                     <ul>
-                        <li>Home</li>
-                        <li>Services</li>
-                        <li>Experience</li>
-                        <li>Portfolio</li>
-                        <li>Testimonials</li>
+                        <li>
+                            <a href="#home">Home</a>
+                        </li>
+                        <li>
+                            <a href="#services">Services</a>
+                        </li>
+                        <li>
+                            <a href="#experience">Experience</a>
+                        </li>
+                        <li>
+                            <a href="#portfolio">Portfolio</a>
+                        </li>
+                        <li>
+                            <a href="#testimonials">Testimonials</a>
+                        </li>
                     </ul>
                 </div>
                 <button className="button">
